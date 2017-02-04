933d7b7fd76441a9d13f7a547be45c5b7378a7fe
Tom Van den Bon <tvdbon@gmail.com>
2017-02-04 22:06:22 +0200

    added test sketch for ota updates

--- part1/code/weatherstationpart1_ota/weatherstationpart1_ota.ino
+++ part1/code/weatherstationpart1_ota/weatherstationpart1_ota.ino
@@ -36,6 +36,9 @@ bool authorized = false;
 
 uint32_t delayMS;
 
+unsigned long previousMillis = 0; 
+const long interval = UPDATEPERIOD * 60 * 1000;  
+
 /*******************************************************
  setup
  *******************************************************/
@@ -49,12 +52,45 @@ void setup() {
   Serial.println(ssid);
 
   //connect to wifi
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+   //Hostname defaults to esp8266-[ChipID]
+   ArduinoOTA.setHostname("weatherstation");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  
 
   Serial.println("");
   Serial.println("WiFi connected");
@@ -77,10 +113,11 @@ void setup() {
     Serial.println( "Authorized!" );
   else
    Serial.println( "Not Authorized!" );
+
+   readSensors();
 }
 
-// main loop
-void loop() 
+void readSensors()
 {
   // read temperature/humidity sensors
   float temp = 0;
@@ -122,8 +159,21 @@ void loop()
     sendSensorValue(HUMIDITYID, humidity);
   }
 
-  // update every x minutes
-  delay(UPDATEPERIOD * 60 * 1000);
+}
+
+// main loop
+void loop() 
+{
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    readSensors();
+  }
+
+  
+  ArduinoOTA.handle();
 }
 
 
